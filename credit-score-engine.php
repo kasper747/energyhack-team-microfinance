4ea7af3ead7fbe6c3f14a7e4f33f063363445dda
pointbazaar <alex23667@gmail.com>
2019-04-06 12:12:47 +0200

    skeleton for credit score engine

---
+++ credit-score-engine.php
@@ -0,0 +1,9 @@
+<?php
+
+    //TODO:
+    //this should be the endpoint to get the credit score for producers and consumers. it should make a JSON
+    //with their current credit score and their credit score over time
+
+    
+
+?>
