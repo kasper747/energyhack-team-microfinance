3b96522ff5613126e8423e1d6dacf90d2754dd12
Justus Janßen <j.janssen@it-jansen.com>
2019-04-06 17:05:52 +0200

    Created ?number= search request

--- api/getMessages.php
+++ api/getMessages.php
@@ -1,58 +1,66 @@
 <?php
+include ('db.php');
+
 header("Content-Type:application/json");
 
 if (isset($_GET['id']) && $_GET['id'] != "")
 	{
-	include ('db.php');
-
-	$en_id = $_GET['id']; //entered email
+	$en_id = $_GET['id'];
 	$status['type'] = "OK";
 	$status['code'] = 200;
 	$status['message'] = "Database is available.";
 	$status['error'] = false;
-	if ($en_id == - 1)
+	$result = mysqli_query($con, "SELECT * FROM `messages` WHERE id='$en_id'");
+	if (mysqli_num_rows($result) > 0)
 		{
-		$result = mysqli_query($con, "SELECT * FROM `messages`");
-		$count = 0;
-		foreach($result as $row)
-			{
-			$user['id'] = $row['id'];
-			$user['message'] = $row['message'];
-			$user['fromNumber'] = $row['fromNumber'];
-			$currentuser[$count] = $user;
-			$data['user'] = $currentuser;
-			$count+= 1;
-			}
+		$row = mysqli_fetch_array($result);
+		$user['id'] = $row['id'];
+		$user['message'] = $row['message'];
+		$user['fromNumber'] = $row['fromNumber'];
+		$currentuser[1] = $user;
+		$data['user'] = $currentuser;
 		}
-	  else
+	}
+elseif (isset($_GET['number']) && $_GET['number'] != "")
+	{
+	$en_number = $_GET['number'];
+	$status['type'] = "OK";
+	$status['code'] = 200;
+	$status['message'] = "Database is available.";
+	$status['error'] = false;
+	$result = mysqli_query($con, "SELECT * FROM `messages` WHERE fromNumber='$en_number'");
+	$count = 0;
+	foreach($result as $row)
 		{
-		$result = mysqli_query($con, "SELECT * FROM `messages` WHERE id='$en_id'");
-		if (mysqli_num_rows($result) > 0)
-			{
-			$row = mysqli_fetch_array($result);
-			$user['id'] = $row['id'];
-			$user['message'] = $row['message'];
-			$user['fromNumber'] = $row['fromNumber'];
-			$currentuser[1] = $user;
-			$data['user'] = $currentuser;
-			}
+		$user['id'] = $row['id'];
+		$user['message'] = $row['message'];
+		$user['fromNumber'] = $row['fromNumber'];
+		$currentuser[$count] = $user;
+		$data['user'] = $currentuser;
+		$count+= 1;
 		}
-
-	$response['status'] = $status;
-	$response['data'] = $data;
 	}
   else
 	{
-	$status['type'] = "Bad Request";
-	$status['code'] = 400;
-	$status['message'] = "The parameters are wrong.";
-	$status['error'] = true;
-	$response['status'] = $status;
-
-	// response($response);
-
+	$status['type'] = "OK";
+	$status['code'] = 200;
+	$status['message'] = "Database is available.";
+	$status['error'] = false;
+	$result = mysqli_query($con, "SELECT * FROM `messages`");
+	$count = 0;
+	foreach($result as $row)
+		{
+		$user['id'] = $row['id'];
+		$user['message'] = $row['message'];
+		$user['fromNumber'] = $row['fromNumber'];
+		$currentuser[$count] = $user;
+		$data['user'] = $currentuser;
+		$count+= 1;
+		}
 	}
 
+$response['status'] = $status;
+$response['data'] = $data;
 response($response);
 
 function response($response)
