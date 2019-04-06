0fd6b3fd4f44aafa7a315d6f433e3a7ed541936c
Justus Janßen <j.janssen@it-jansen.com>
2019-04-06 16:08:39 +0200

    .htaccess entfernt ):

--- api/readme.txt
+++ api/readme.txt
@@ -1,9 +1,11 @@
 How to trigger the API:
 
 Get all Messages who are in the Database:
-http://it-jansen.com/api/v1/getMessages/?id=-1
+http://it-jansen.com/api/v1/getMessages.php?id=-1
 
 Get a specific Message:
-http://it-jansen.com/api/v1/getMessages/?id= -> Insert the ID
+http://it-jansen.com/api/v1/getMessages.php?id= -> Insert the ID
 
+Insert SMS Data
+http://it-jansen.com/api/v1/sms.php?text=Test&number=01753271881
 
--- api/sms.php
+++ api/sms.php
@@ -1,44 +1,55 @@
 <?php
 header("Content-Type:application/json");
-if (isset($_GET['text']) && $_GET['text'] != "" && isset($_GET['number']) && $_GET['number'] != "") {
-    include('db.php');
-    $text    = $_GET['text'];
-    $number = $_GET['number'];
-    $result      = mysqli_query($con, "INSERT INTO messages (message, fromNumber) VALUES ('$text', '$number')");
-    if ($result === TRUE) {
-        $status['type']    = "OK";
-        $status['code']    = 200;
+
+if (isset($_GET['text']) && $_GET['text'] != "" && isset($_GET['number']) && $_GET['number'] != "")
+	{
+	include ('db.php');
+
+	$text = $_GET['text'];
+	$number = $_GET['number'];
+	$result = mysqli_query($con, "INSERT INTO messages (message, fromNumber) VALUES ('$text', '$number')");
+	if ($result === TRUE)
+		{
+		$status['type'] = "OK";
+		$status['code'] = 200;
 		$status['message'] = "Message added into the database.";
-        $status['error']   = false;
-		
-		//$response['status'] = $status;
-      
-        mysqli_close($con);
-    } else {
-		$status['type']    = "Not Found";
-        $status['code']    = 404;
-        $status['message'] = "Message not added into the database.";
-        $status['error']   = true;
-        
-        //$response['status'] = $status;
-        //response($response);
-    }
-} else {
-    $status['type']    = "Bad Request";
-    $status['code']    = 400;
-    $status['message'] = "The parameters are wrong.";
-    $status['error']   = true;
-    
-    //$response['status'] = $status;
-    //response($response);
-}
+		$status['error'] = false;
+
+		// $response['status'] = $status;
+
+		mysqli_close($con);
+		}
+	  else
+		{
+		$status['type'] = "Not Found";
+		$status['code'] = 404;
+		$status['message'] = "Message not added into the database.";
+		$status['error'] = true;
+
+		// $response['status'] = $status;
+		// response($response);
+
+		}
+	}
+  else
+	{
+	$status['type'] = "Bad Request";
+	$status['code'] = 400;
+	$status['message'] = "The parameters are wrong.";
+	$status['error'] = true;
+
+	// $response['status'] = $status;
+	// response($response);
+
+	}
 
 $response['status'] = $status;
 response($response);
 
 function response($response)
-{
-    $json_response = json_encode($response);
-    echo $json_response;
-}
+	{
+	$json_response = json_encode($response);
+	echo $json_response;
+	}
+
 ?>
