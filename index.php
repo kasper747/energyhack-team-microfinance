8ec07dcf6663027d22d962bd5123b82b21ed55ce
pointbazaar <alex23667@gmail.com>
2019-04-06 13:00:02 +0200

    this commit contains errors,

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
+<html>
+
+<body>
 <?php
 
     //TODO: deliver the .html that contains the vue.js stuff for our main page
     echo("hello");
+    if( isset($_COOKIE["username"] ){
+        echo("logged in as: " . $_COOKIE["username"] );
+    }else{
+        echo("Not Logged in ");
+    }
 ?>
+
+
+<p>LOGIN/ Change User</p>
+
+<form method="POST" action="/login.php">
+    <select name="username">
+        <option>Consumer1</option>
+        <option>Consumer1</option>
+    </select>
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <?php
     //TODO: make a POST endpoint here to change the 'user' cookie to one of the predefined users
     //depending on the parameters
+    if( $_SERVER['REQUEST_METHOD']=="POST" ){
+
+        header("Location: /index.php");
+    }else{
+        echo("use POST");
+    }
 ?>
