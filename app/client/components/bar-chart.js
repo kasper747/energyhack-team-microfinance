2376d1544df8c6235e4b9f8add27d1bfbd61e1e6
Unknown <andrey.bogomolov@tryggel.com>
2019-06-15 23:24:12 +0200

    Rendering the grafic with new data

--- app/client/components/bar-chart.js
+++ app/client/components/bar-chart.js
@@ -1,18 +1,79 @@
 import VueChartJs from 'vue-chartjs'
 
+var options = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      },
+      gridLines: {
+        display: true
+      }
+    }],
+    xAxes: [{
+      gridLines: {
+        display: false
+      }
+    }]
+  },
+  legend: {
+    display: false
+  },
+  responsive: true,
+  maintainAspectRatio: false
+};
+
 export default {
   extends: VueChartJs.Line,
-  props: ['data', 'options','labels'],
+  props: ['data_prod', 'data_load', 'options', 'labels'],
   mounted() {
-    this.renderChart({
-      labels: this.labels,
-      datasets: [
-        {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: this.data
-        }
-      ]
-    }, {responsive: true, maintainAspectRatio: false})
+    this.draw_chart();
+  },
+  methods: {
+    draw_chart: function () {
+      this.renderChart({
+            labels: this.labels,
+            datasets: [
+              {
+                label: 'Production',
+                backgroundColor: 'rgba(231, 0, 255, 0)',
+                borderColor: '#fc6f77',
+                data: this.data_prod
+              },
+              {
+                label: 'Load',
+                backgroundColor: 'rgba(0, 231, 255, 0)',
+                borderColor: '#646afc',
+                data: this.data_load
+              },
+            ]
+          }, {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+              elements: {line: {fill: false}},
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                },
+
+                gridLines: {
+                  display: true
+                }
+              }],
+              xAxes: [{
+                gridLines: {
+                  display: false
+                }
+              }]
+            },
+          }
+      )
+    }
+  },
+  watch: {
+    data_prod: function () {
+      this.draw_chart();
+    }
   }
 }
--- app/client/pages/app/forecast.vue
+++ app/client/pages/app/forecast.vue
@@ -1,8 +1,20 @@
-<template>
+<!--https://codepen.io/apertureless/pen/zEvvWM-->
 
-    <BarChart :data="[40, 39, 10, 40, 39, 80, 40]"
-              :labels="['January', 'February', 'March', 'April', 'May', 'June', 'July']"
-    />
+<template>
+    <v-app>
+        <v-sheet>
+            <v-btn @click="fetch_data">
+                Get data
+            </v-btn>
+            <BarChart
+                      :data_prod="data_prod"
+                      :data_load="data_load"
+                      :labels="labels"
+            />
+        </v-sheet>
+    </v-app>
+
+
 </template>
 
 <script>
@@ -14,10 +26,22 @@
     components: {
       BarChart,
     },
+
     data() {
-      return {}
+      return {
+        data_prod: [],
+        data_load: [],
+        labels: [],
+      }
+    },
+    methods: {
+      fetch_data: function () {
+        // alert('test');
+        this.data_prod = [40, 39, 10, 40, 39, 80, 40];
+        this.data_load = [1, 40, 39, 10, 40, 39, 80];
+        this.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+      }
     }
-    ,
   }
 </script>
 
